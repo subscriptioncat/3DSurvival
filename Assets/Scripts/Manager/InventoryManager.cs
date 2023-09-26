14d1911d0126dc608dadbe743935e258bc682d1d
jyo217 <solbaram217@gmail.com>
2023-09-26 17:57:13 +0900

    [ADD] Item, 아이템 수량 텍스트 관련 기능 추가
    Inventory 에서 저장하는 아이템 정보를 Item 으로 변경. Item 은 ItemSO 와 수량 정보를 가진다.

---
+++ Assets/Scripts/Manager/InventoryManager.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager instance;
+    public List<Item> items;
+
+    [SerializeField] private ItemPopupManager popup;
+    [SerializeField] private Transform slotParent;
+    [SerializeField] private InventorySlot[] slots;
+
+    private void OnValidate()
+    {
+        slots = slotParent.GetComponentsInChildren<InventorySlot>();
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        if (popup == null)
+        {
+            popup = GetComponent<ItemPopupManager>();
+        }
+        UpdateSlot();
+    }
+
+    public void UpdateSlot()
+    {
+        int i = 0;
+        //현재까지 슬롯에 추가한 아이템의 갯수가 슬롯의 한도나 아이템 리스트의 한도 이하일 때 까지 추가한다.
+        for (; i < items.Count && i < slots.Length; i++)
+        {
+            slots[i].Item = items[i];
+        }
+        //인벤토리의 공간이 아직 남았다면 빈 칸으로 채운다.
+        for (; i < slots.Length; i++)
+        {
+            slots[i].Item = null;
+            slots[i].UpdateQuantity();
+        }
+    }
+
+    /// <summary>
+    /// 아이템 추가. 인벤토리에 이미 존재하는 종류의 아이템이라면 갯수만 증가.
+    /// </summary>
+    public void AddItem(Item item)
+    {
+        if (item.Quantity <= 0)
+        {
+            Debug.Log("아이템의 갯수는 1 개 이상이어야 합니다!!!");
+            return;
+        }
+
+        //인벤토리에 존재하는 아이템이라면
+        for (int i = 0; i < slots.Length ; i++)
+        {
+            if(slots[i].Item.ItemSO.itemName == item.ItemSO.itemName)
+            {
+                slots[i].SetQuantity(slots[i].Item.Quantity + item.Quantity);
+                return;
+            }
+        }
+
+        //인벤토리에 존재하지 않는 아이템이고, 인벤토리 공간이 남아있다면
+        if (items.Count < slots.Length)
+        {
+            items.Add(item);
+            UpdateSlot();
+        }
+        else
+        {
+            Debug.Log("인벤토리가 가득 찼습니다!!!");
+        }
+    }
+
+    /// <summary>
+    /// 해당 아이템의 수량 반환. 없는 아이템이라면 0 반환.
+    /// </summary>
+    public int GetQuantity(string itemName)
+    {
+        for (int i = 0; i < items.Count ; i++)
+        {
+            if(items[i].ItemSO.itemName == itemName)
+            {
+                return items[i].Quantity;
+            }
+        }
+
+        return 0;
+    }
+
+    /// <summary>
+    /// 아이템 버리기, 제작 시 팝업창을 띄울 때 사용할 메소드. 경우에 따라 파기할 수도 있음
+    /// </summary>
+    /// <param name="okCallback"></param>
+    /// <param name="text"></param>
+    /// <param name="lable"></param>
+    public void ShowPopup(Action okCallback, string text, string lable)
+    {
+        //popup.ShowPopup(okCallback, text, lable);
+    }
+}
