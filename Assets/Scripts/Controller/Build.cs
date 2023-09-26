552c7775cff74f459fc9ca1671deee407d86804b
KimDoHyeon <hj2471755@naver.com>
2023-09-26 15:14:33 +0900

    Build.cs 임시 업로드

--- Assets/Scripts/Controller/Build.cs
+++ Assets/Scripts/Controller/Build.cs
@@ -12,4 +12,28 @@ public class Build : MonoBehaviour
     //모든 건축 리스트를 저장해야 됩니다.
     [SerializeField] private List<BuildSO> buildList;
     public List<BuildSO> BuildList { get { return buildList; } }
+
+    public bool IsBuildingMaterials(int selectIndex)
+    {
+        int nowIndex = buildList[selectIndex].ConsumingResources.Count;
+        for (int i = 0; i < nowIndex; i++)
+        {
+            //if(!Inventory.instance.TryConsume(
+            //    buildList[selectIndex].ConsumingResources[i].itemName,
+            //    buildList[selectIndex].ConsumingResources[i].numberConsumed
+            //    ))
+            //{
+            //    return false;
+            //}
+        }
+        return true;
+    }
+    //public bool IsCollision()
+    //{
+
+    //}
+    //public void Build()
+    //{
+    //    //
+    //}
 }
