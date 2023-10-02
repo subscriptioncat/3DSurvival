afd20a3357253f8e24ce299ba832fc3aae276586
KimDoHyeon <hj2471755@naver.com>
2023-10-02 11:08:09 +0900

    병합을 위한 임시저장

--- Assets/Scripts/Controller/Build.cs
+++ Assets/Scripts/Controller/Build.cs
@@ -1,6 +1,8 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static BuildSO;
 
 public enum E_BuildingName
 {
@@ -15,16 +17,51 @@ public class Build : MonoBehaviour
 
     [Range(0, 20)][SerializeField] private int previewDistance;
 
-    private int nowIndex;
+    [Range(0f, 100f)][SerializeField] private int MaxPosX;
+    [Range(0f, -100f)][SerializeField] private int MinPosX;
+    [Range(0f, 100f)][SerializeField] private int MaxPosY;
+    [Range(0f, -100f)][SerializeField] private int MinPosY;
+
+    private int selectIndex;
     private GameObject preViewGameObject;
     private Transform preViewGameObjectTransform;
     private PreviewObject preViewComponent;
     private bool isPreViewActive = false;
 
-    public bool IsBuildingMaterials(int selectIndex)
+    //팝업창에서 이름 및 소모하는 재료를 보여주기위해 buildList를 모두 전달
+    public List<BuildSO> GetConsumingResources()
     {
-        nowIndex = buildList[selectIndex].ConsumingResources.Count;
-        for (int i = 0; i < nowIndex; i++)
+        return buildList;
+    }
+
+    //팝업창에서 선택하면 호출
+    public void OnPreView(int _selectIndex)
+    {
+        if (IsBuildingMaterials(_selectIndex))
+        {
+            DisplayPreView();
+        }
+        else
+        {
+            //재료가 부족합니다.
+        }
+    }
+
+    //마우스 좌클릭하면 호출
+    public void OnBuild()
+    {
+        if (isPreViewActive) 
+        {
+
+        }
+    }
+
+    //재료가 있는지 확인
+    public bool IsBuildingMaterials(int _selectIndex)
+    {
+        selectIndex = _selectIndex;
+        int maxIndex = buildList[selectIndex].ConsumingResources.Count;
+        for (int i = 0; i < maxIndex; i++)
         {
             //if(!Inventory.instance.TryConsume(
             //    buildList[selectIndex].ConsumingResources[i].itemName,
@@ -36,68 +73,18 @@ public class Build : MonoBehaviour
         }
         return true;
     }
-    void DisplayExample()
+
+    //PreView를 표시합니다.
+    void DisplayPreView()
     {
         isPreViewActive = true;
-        preViewGameObject = Instantiate(buildList[nowIndex].PreViewPrefab, transform.position + transform.forward * previewDistance, Quaternion.identity);
+        preViewGameObject = Instantiate(buildList[selectIndex].PreViewPrefab, transform.position + transform.forward * previewDistance, Quaternion.identity);
         preViewGameObjectTransform = preViewGameObject.transform;
         preViewComponent = preViewGameObject.GetComponent<PreviewObject>();
     }
+    //건축물을 생성합니다.
     private void InstantiateBuilding()
     {
-        Instantiate(buildList[nowIndex].Prefab, transform.position + transform.forward * previewDistance, Quaternion.identity);
-    }
-
-    //테스트를 위한 코드~
-    private void Start()
-    {
-        DisplayExample();
-    }
-    private void Update()
-    {
-        if (isPreViewActive)
-        {
-            preViewGameObjectTransform.position = transform.position + transform.forward * previewDistance;
-            preViewGameObjectTransform.rotation = transform.rotation;
-        }
-        
-    }
-    //까지
-
-
-    public void OnPreView()
-    {
-        if (IsBuildingMaterials(0))
-        {
-            DisplayExample();
-        }
-        else
-        {
-            //자재가 부족합니다.
-        }
-    }
-
-    public void OnBuild()
-    {
-        if (isPreViewActive)
-        {
-            if (IsBuildingMaterials(0))
-            {
-                if (preViewComponent.isBuildable())
-                {
-                    InstantiateBuilding();
-                }
-            }
-            else
-            {
-                OnExitPreView();
-                //자재가 부족합니다.
-            }
-        }
-    }
-    public void OnExitPreView()
-    {
-        isPreViewActive = false;
-        Destroy(preViewGameObject);
+        Instantiate(buildList[selectIndex].Prefab, transform.position + transform.forward * previewDistance, Quaternion.identity);
     }
 }
